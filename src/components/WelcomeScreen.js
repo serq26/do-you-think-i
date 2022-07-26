74a099d4ed915f604cb5b4a1ed20576e64a680d6
serq26 <serhat26basak@gmail.com>
2022-07-26 13:15:27 +0300

    Style Fixes

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -11,21 +11,21 @@ export default function WelcomeScreen() {
             <span className="sm:block">Enjoy Your Time.</span>
           </h1>
 
-          <p className="max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl">
+          <p className="max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl dark:text-white text-gray-500">
           In order to have a fun time, you should first <br/>
           register or login as a member.
           </p>
 
           <div className="flex flex-wrap justify-center gap-4 mt-8">
             <Link
-              className="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring"
+              className="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-blue-500 focus:outline-none focus:ring"
               to="/signin"
             >
               Signin
             </Link>
 
             <Link
-              className="block w-full px-12 py-3 text-sm font-medium text-white  bg-blue-600 border border-blue-600 rounded sm:w-auto hover:bg-transparent active:bg-blue-500 focus:outline-none focus:ring"
+              className="block w-full px-12 py-3 text-sm font-medium text-white  bg-blue-600 border border-blue-600 rounded sm:w-auto hover:bg-transparent active:bg-blue-500 hover:text-blue-500 focus:outline-none focus:ring"
               to="/login"
             >
               Login
